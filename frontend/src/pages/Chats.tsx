c77d047556af89da5aac104105e2971b285c1eb5
Ashu Negi <anegi6723@gmail.com>
2025-06-11 00:51:28 +0530

    sending roomId in props to ChatWindow

--- frontend/src/pages/Chats.tsx
+++ frontend/src/pages/Chats.tsx
@@ -1,11 +1,14 @@
+import { useParams } from "react-router-dom"
 import ChatList from "../components/ChatList"
 import ChatWindow from "../components/ChatWindow"
 
 function Chats() {
+  const {roomId} = useParams();
+
   return (
     <div className="mt-18 flex items-center w-full" style={{height : "calc(100vh - 72px)"}}>
         <ChatList />
-        <ChatWindow />
+        <ChatWindow roomId={roomId || ""} />
     </div>
   )
 }
