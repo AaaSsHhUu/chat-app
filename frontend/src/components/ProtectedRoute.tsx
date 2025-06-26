a27a5399901e514da09ad58540d3e1c4cbf088d7
Ashu Negi <anegi6723@gmail.com>
2025-06-26 11:51:31 +0530

    feat(ProtectedRoute): enhance authentication handling with loading state and user management

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,41 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import type { RootState } from "@/app/store";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { clearUser, setUser } from "@/features/auth/authSlice";
+import { auth } from "@/firebase";
+import Loader from "./Loader";
 
 function ProtectedRoute() {
-    const { user } = useSelector((state : RootState) => state.auth);
+    const dispatch = useDispatch();
+    const [checkingAuth, setCheckingAuth] = useState(true);
+    const currentUser = useSelector((state: any) => state.auth.user);
+    
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                dispatch(
+                    setUser(user)
+                );
+            } else {
+                dispatch(clearUser());
+            }
+            setCheckingAuth(false);
+        });
 
-    return user ? <Outlet /> : <Navigate to={"/login"} />
+        return () => unsubscribe();
+    }, [dispatch])
+
+    if (checkingAuth) {
+        return <Loader />;
+    }
+
+    if (!currentUser) {
+        return <Navigate to="/login" />;
+    }
+
+    return <Outlet />;
 }
 
-export default ProtectedRoute;
+export default ProtectedRoute
