09e6bab39b131019f2af732a7335e85eb873fa5b
Ashu Negi <anegi6723@gmail.com>
2025-07-03 23:25:48 +0530

    bug fix - ws auth

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,41 +1,19 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { WebSocketProvider } from "@/context/WebSocketContext";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { clearUser, setUser } from "@/features/auth/authSlice";
-import { auth } from "@/firebase";
-import Loader from "./Loader";
+import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoute() {
-    const dispatch = useDispatch();
-    const [checkingAuth, setCheckingAuth] = useState(true);
     const currentUser = useSelector((state: any) => state.auth.user);
-    
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                dispatch(
-                    setUser(user)
-                );
-            } else {
-                dispatch(clearUser());
-            }
-            setCheckingAuth(false);
-        });
-
-        return () => unsubscribe();
-    }, [dispatch])
-
-    if (checkingAuth) {
-        return <Loader />;
-    }
 
     if (!currentUser) {
         return <Navigate to="/login" />;
     }
 
-    return <Outlet />;
+    return (
+        <WebSocketProvider user={currentUser}>
+            <Outlet />
+        </WebSocketProvider>
+    )
 }
 
 export default ProtectedRoute
--- frontend/src/context/WebSocketContext.tsx
+++ frontend/src/context/WebSocketContext.tsx
@@ -1,21 +1,23 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, type User } from "firebase/auth";
 import { createContext, useContext, useEffect, useRef, useState } from "react";
 
 const WebSocketContext = createContext<WebSocket | null>(null);
 
-export const WebSocketProvider = ({ children }: { children: React.ReactNode }) => {
+export const WebSocketProvider = ({ user, children }: { user : User, children: React.ReactNode }) => {
     const socketRef = useRef<WebSocket | null>(null); // You don’t want React to re-render the component whenever the socket changes (which useState would do).
     const [socket, setSocket] = useState<WebSocket | null>(null);
 
     useEffect(() => {
         const connectSocket = async () => {
+            if(!user) return;
+            
+            const token = await user?.getIdToken();
+            
             socketRef.current = new WebSocket("ws://localhost:8080");
-
-            const auth = getAuth();
-            const token = await auth.currentUser?.getIdToken();
-
+            
             socketRef.current.onopen = () => {
                 if (token) {
+                    console.log("sending auth");
                     socketRef.current?.send(JSON.stringify({
                         type: "auth",
                         payload: { token }
